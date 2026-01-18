512d5dfbffd1088d71d4a73cc37c6c0583c38813
Md-Faizan-Rasheed <s1391792@gmail.com>
2026-01-19 01:27:24 +0530

    add cors

--- main.py
+++ main.py
@@ -39,6 +39,8 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://your-frontend.vercel.app"  # production frontend
+
     ],
     allow_credentials=True,
     allow_methods=["*"],
